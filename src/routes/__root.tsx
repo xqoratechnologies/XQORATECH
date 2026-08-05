1a7bee9dd296876b93291b6a359d2e9dcb6de6aa
Ketan <gehlotchetan86@gmail.com>
2026-08-05 22:05:50 +0530

    added logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/logo.png", type: "image/png" },
+      { rel: "icon", href: "/favicon.png", type: "image/png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
